c2c4ba1a2e7aeb1ae8e28ee59f1ee7304f753dce
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-02-11 03:11:41 +0900

    各マスのアンパンをひっくり返せるようになった。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,25 @@
-// アンパンの画像パス
-const IMG_PATH01 = '/anpan.png'
-
 import React, { useState } from 'react';
 
 const TwoDimensionalArray: React.FC = () => {
-  const twoDimensionalArray: number[][] = [];
-  const [clicked, setClicked] = useState<number[][]>(Array(8).fill(Array(8).fill(0)));
+  const [twoDimensionalArray, setTwoDimensionalArray] = useState<number[][]>([]);
+  const [activeImage, setActiveImage] = useState<number>(0);
+  const [imgPath1, setImgPath1] = useState<string>('/anpan.png');
+  const [imgPath2, setImgPath2] = useState<string>('/anpan_gray.png');
 
-  for (let i = 0; i < 8; i++) {
-    twoDimensionalArray[i] = [];
-    for (let j = 0; j < 8; j++) {
-      twoDimensionalArray[i][j] = 0;
+  // 2次元配列の初期化
+  if (twoDimensionalArray.length === 0) {
+    for (let i = 0; i < 8; i++) {
+      twoDimensionalArray[i] = [];
+      for (let j = 0; j < 8; j++) {
+        twoDimensionalArray[i][j] = 0;
+      }
     }
   }
 
-  twoDimensionalArray[3][3] = 0;
-  twoDimensionalArray[3][4] = 1;
-  twoDimensionalArray[4][3] = 1;
-  twoDimensionalArray[4][4] = 0;
-
   const handleClick = (i: number, j: number) => {
-    setClicked((prevState) => {
-      const newState = JSON.parse(JSON.stringify(prevState));
-      newState[i][j] = prevState[i][j] === 1 ? 0 : 1;
-      return newState;
-    });
+    const updatedArray = [...twoDimensionalArray];
+    updatedArray[i][j] = updatedArray[i][j] === 0 ? 1 : 0;
+    setTwoDimensionalArray(updatedArray);
   };
 
   return (
@@ -34,23 +29,17 @@ const TwoDimensionalArray: React.FC = () => {
           {twoDimensionalArray.map((row, i) => (
             <tr key={i}>
               {row.map((col, j) => (
-                <td key={j} onClick={() => handleClick(i, j)}>
-                  {clicked[i][j] === 1 ? (
-                    <div className="grid-item-active">
-                      <img
-                        src={IMG_PATH01}
-                        alt="your-image"
-                        style={{
-                          display: 'block',
-                          margin: '0 auto',
-                          maxWidth: '100%',
-                          maxHeight: '100%',
-                        }}
-                      />
-                    </div>
-                  ) : (
-                    <div className="grid-item-inactive" />
-                  )}
+                <td key={j}>
+                  <div
+                    className={col === 1 ? 'grid-item-active' : 'grid-item-inactive'}
+                    onClick={() => handleClick(i, j)}
+                  >
+                    <img
+                      src={col === 0 ? imgPath1 : imgPath2}
+                      alt="grid-item"
+                      style={{ width: '100%', height: '100%' }}
+                    />
+                  </div>
                 </td>
               ))}
             </tr>
@@ -60,4 +49,5 @@ const TwoDimensionalArray: React.FC = () => {
     </div>
   );
 };
+
 export default TwoDimensionalArray;
