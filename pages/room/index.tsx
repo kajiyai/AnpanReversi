56e8ac5e232921e893e638c0d1d8f219d960d814
masa5555 <masa57887@gmail.com>
2023-02-12 15:03:07 +0900

    push

--- pages/room/index.tsx
+++ pages/room/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Flex, Heading, Input, Text, Box } from '@chakra-ui/react';
+import axios from 'axios';
 
 const IndexPage = () => {
+  const [inputUserName, setInputUserName] = useState<string>('')
+  const [inputCreateRoomName, setInputCreateRoomName] =   useState<string>('')
+  const [inputJoinRoomName, setInputJoinRoomName] = useState<string>('')
+
+  const createRoom = () => {
+    axios.post('http://localhost:8000/api/v1/create', {
+      name: inputUserName,
+      room_id: inputCreateRoomName,
+    })
+    .then(() => {
+      
+    })
+  }
+
+  const joinRoom = () => {
+
+  }
+
   return  (
     <Flex height="100vh" alignItems="center" justifyContent="center">
       <Flex direction="column" background="blackAlpha/700" p={12} rounded={0}>
@@ -10,15 +29,18 @@ const IndexPage = () => {
           さあゲームを始めましょう！
         </Heading>
 
+        <Text fontSize='lg' mb={4}>ユーザ名</Text>
+        <Input placeholder="ユーザ名を入力してください" variant="flushed" type="text" padding={16} rounded={4} value={inputUserName} onChange={(e) => setInputUserName(e.target.value)}/>
+
         <Text fontSize='lg' mb={4}>ルームを作成する</Text>
-        <Input placeholder="お好きなルーム名を入力してください" variant="flushed" type="email" padding={16} rounded={4}/>
-        <Box as='button' borderRadius='xl' bg='orange' color='white' px={50} h={56} padding={16} mb={40} rounded={4} >
+        <Input placeholder="ルームID" variant="flushed" type="email" padding={16} rounded={4} value={inputCreateRoomName} onChange={(e) => {setInputCreateRoomName(e.target.value)}}/>
+        <Box as='button' borderRadius='xl' bg='orange' color='white' px={50} h={56} padding={16} mb={40} rounded={4}  onClick={createRoom()}>
           作成
         </Box>
 
         <Text fontSize='lg' mb={4}>ルームに入る</Text>
-        <Input placeholder="相手のルーム名を入力してください" variant="flushed" type="email" padding={16} rounded={4}/>
-        <Box as='button'  borderRadius='xl' bg='green' color='white' px={50} h={56} padding={16} mb={16} rounded={4}>
+        <Input placeholder="ルームID" variant="flushed" type="email" padding={16} rounded={4} value={inputJoinRoomName} onChange={(e) => {setInputJoinRoomName(e.target.value)}}/>
+        <Box as='button'  borderRadius='xl' bg='green' color='white' px={50} h={56} padding={16} mb={16} rounded={4} onClick={joinRoom()}>
           検索
         </Box>
 
